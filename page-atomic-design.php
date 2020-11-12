ecc4cf21d948f21b6602e1dff60228e213b27eb5
diegovaz98 <diego@oni.com.br>
2020-11-12 13:00:53 -0300

    Adiciona a página "atomic design" e alguns estilos referentes a ela

---
+++ page-atomic-design.php
@@ -0,0 +1,90 @@
+<?php
+
+get_header(); ?>
+
+
+<section class="container ">
+    <div class="escala4">Atomic Elements</div>
+    <div class="row background_white">
+        <div class="col-md-6 p-5"> 
+            <div class="escala4">
+                    Título
+            </div>
+            <div class="escala1">
+                <code data-lang="html"> 
+                    <pre>&lt;div class="escala4"&gt;Título&lt;/div&gt;</pre>
+                </code>
+            </div>
+            <div class="escala2">
+                Subtítulo
+            </div>
+            <div class="escala1 ">
+                <code data-lang="html"> 
+                    <pre>&lt;div class="escala2"&gt;Subtítulo&lt;/div&gt;</pre>
+                </code>
+            </div>
+
+            <div class="escala1">
+                Texto
+            </div>
+            <div class="escala1">
+                <code data-lang="html"> 
+                    <pre>&lt;div class="escala1"&gt;Texto&lt;/div&gt;</pre>
+                </code>
+            </div>
+        </div>
+        <div class="col-md-6 p-5">
+            <div class="mb-3" >
+                <img class="image-profile" src="https://files.monday.com/photos/15813062/thumb/15813062-Diego_Fernandes_photo_2020_08_31_20_26_10.png?1598905570"/>
+                <img class="image-profile" src="https://files.monday.com/photos/4499078/thumb/4499078-Marcus_photo_2018_07_30_21_21_58.png?1532985718"/>
+            </div>
+            <div class="escala1">
+                <code data-lang="html"> 
+                    <pre>&lt;img class="image-profile" src="..."&gt;</pre>
+                </code>
+            </div>
+            <div class="mb-3">
+                <button type="button" class="btn btn-outline-dark">Editar Perfil</button>
+            </div>
+            <div class="escala1">
+                <code data-lang="html"> 
+                    <pre>&lt;button type="button" class="btn btn-outline-dark"&gt;Editar Perfil&lt;/button&gt;</pre>
+                </code>
+            </div>
+        </div>
+        
+    </div>
+    <div class="row background_white"  >
+
+        <div class="col-md-6 pl-5 pr-5 ">
+            <div class="d-flex ">
+                <p class="competency-sphere background_grey mr-2" />
+                <p class="competency-sphere background_green" />
+            </div>
+            <div class="escala1">
+                <code data-lang="html"> 
+                    <pre>&lt;p class="competency-sphere background_grey" /&gt;</pre>
+                </code>
+                <code data-lang="html"> 
+                    <pre>&lt;p class="competency-sphere background_green" /&gt;</pre>
+                </code>
+            </div>
+            
+        </div>
+        
+
+    </div>
+
+    
+</section>
+
+
+
+
+
+<?php
+              
+get_footer();
+
+?>
+
