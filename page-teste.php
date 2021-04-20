3e516884a923cfba2331f75790e6a6fb6a5d24e2
Thiago <thiago@oni.com.br>
2021-04-20 18:05:23 -0300

    Resolvendo a criação de missões no clickup, travado no assignee e time estimate que fica vazio[

--- page-teste.php
+++ page-teste.php
@@ -5,15 +5,36 @@
 <?php get_header(); ?>
 
 <?php
-$request = get_transient('request');
+$criamissao = get_transient('criamissoes');
 echo "<pre>";
-var_dump($request);
+var_dump($criamissao);
+echo "</pre>";
+// 
+echo "<pre>";
+var_dump(clickup::clickMissoesGestao($criamissao[0], $criamissao[1], $criamissao[2]));
 echo "</pre>";
 
-$table_records_frentes = get_transient('table_records_frentes');
+
+
+/*
+$today = DateTime::createFromFormat('d/m/Y', '17/03/2021');
+echo $today->getTimestamp(); # or $dt->format('U');
+$today = $today->getTimestamp();
+
 echo "<pre>";
-var_dump($table_records_frentes);
+var_dump($today);
 echo "</pre>";
+
+$tomorrow = strtotime('tomorrow');
+echo "<pre>";
+var_dump($tomorrow);
+echo "</pre>";
+
+echo "<pre>";
+var_dump(clickup::clickCriaList('lista',1614611719,1615994119,'3107782', '35709302') );
+echo "</pre>";
+*/
+
 
 ?>
 <?php get_footer(); ?>
